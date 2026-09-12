94fb2dff062c33e89abefa9be279d3dee2e278e9
mariafidel <mariafidelregueros@gmail.com>
2026-09-12 07:03:06 +0200

    Delete fidel-regueros-website directory

--- fidel-regueros-website/script.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var nav = document.getElementById('siteNav');
-    function onScroll(){
-      if(window.scrollY > 40){ nav.classList.add('solid'); }
-      else{ nav.classList.remove('solid'); }
-    }
-    window.addEventListener('scroll', onScroll, {passive:true});
-    onScroll();
-  })();
